5a8b657ed410060b89187e5af72f3c1c235dc236
Jazel Lim <lim.jazel@gmail.com>
2024-03-16 21:28:15 +0800

    Minor styling fix for banner

--- components/home/TheBanner.vue
+++ components/home/TheBanner.vue
@@ -13,7 +13,7 @@
 		class="banner [ px-6 md:px-12 pb-12 md:pb-16 ] [ flex flex-col items-center ]">
 		<div class="[ relative ] [ flex flex-col lg:flex-row items-center ]">
 			<div
-				class="[ lg:-ml-20 lg:mr-60 ] [ relative -bottom-32 md:-bottom-48 lg:-bottom-12 ] [ max-w-full md:max-w-xl lg:max-w-5xl ]">
+				class="[ lg:mr-60 ] [ relative -bottom-32 md:-bottom-48 lg:-bottom-12 ] [ max-w-full md:max-w-xl lg:max-w-3xl ]">
 				<img
 					src="/images/phone-app.jpeg"
 					alt=""
@@ -23,9 +23,9 @@
 			<div
 				class="decorative-group [ absolute top-[5%] md:top-6 lg:top-[20%] lg:right-0 ]">
 				<div
-					class="contacts-decorative [ mt-12 md:mt-16 lg:mt-28 ] [ text-white text-[0.5rem] lg:text-xs ]">
+					class="contacts-decorative [ mt-12 md:mt-16 lg:mt-28 ] [ text-white text-[0.5rem] lg:text-[0.65rem] ]">
 					<span>Contacts</span>
-					<ul class="[ mt-2 ]">
+					<ul class="[ mt-2 ] [ leading-loose ]">
 						<li v-for="person in people">
 							{{ person }}
 						</li>
@@ -34,7 +34,7 @@
 
 				<div class="popup-menu-decorative">
 					<div
-						class="[ max-w-24 md:max-w-32 lg:max-w-48 ] [ shadow-lg bg-neutral-100/70 ]">
+						class="[ max-w-24 md:max-w-32 lg:max-w-40 ] [ shadow-lg bg-neutral-100/70 ]">
 						<img
 							src="/images/popup-menu.png"
 							alt=""
@@ -78,7 +78,7 @@
 	}
 
 	.contacts-decorative {
-		@apply p-3 md:p-5 border opacity-55;
+		@apply p-3 md:py-2 md:px-4 border opacity-55;
 		box-sizing: border-box;
 	}
 </style>
