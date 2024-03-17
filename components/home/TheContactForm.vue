f3e72f290410613e998842d598406c0547ea98e2
Jazel Lim <lim.jazel@gmail.com>
2024-03-17 19:37:42 +0800

    Add styling to contact section description

--- components/home/TheContactForm.vue
+++ components/home/TheContactForm.vue
@@ -55,15 +55,33 @@
 			<div class="[ flex flex-wrap lg:flex-nowrap gap-16 md:gap-12 ]">
 				<div
 					class="[ lg:mr-12 ] [ grid content-start ] [ w-full md:w-1/2 lg:min-w-1/3 ]">
-					<h2 class="[ font-bold ]">Book your demo today</h2>
-					<p class="[ mt-12 ]">
-						“Kudosity was the easiest and most effective marketing tool we’ve
-						ever used for lead qualification at Uber.”
-					</p>
+					<h2 class="[ font-bold text-white ]">Book your demo today</h2>
 
-					<span class="[ mt-6 ]">Uber, Singapore</span>
+					<div class="[ mt-12 ] [ relative ]">
+						<span class="[ absolute ] [ opacity-15 ]">
+							<svg
+								width="155"
+								height="135"
+								viewBox="0 0 155 135"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M59.785 27.7397C40.654 34.4397 30.61 49.7401 30.61 70.3081H59.785V134.876H0V68.876C0 37.787 22.958 11.4819 59.785 0.00292969V27.7397ZM154.964 27.7397C135.354 34.9147 125.788 49.7401 125.788 70.3081H154.964V134.876H95.178V68.876C95.178 39.223 117.658 11.0039 154.964 0.00292969V27.7397Z"
+									fill="#ACC6CC" />
+							</svg>
+						</span>
+
+						<p class="[ mt-20 px-4 ] [ text-white/70 ] [ relative z-10 ]">
+							“Kudosity was the easiest and most effective marketing tool we’ve
+							ever used for
+							<span class="[ font-bold ]">lead qualification</span> at Uber.”
+						</p>
+					</div>
+
+					<span class="[ mt-6 px-4 ] [ text-accent ]">Uber, Singapore</span>
 				</div>
 
+				<!-- Form -->
 				<div class="[ xl:px-12 ] [ w-full ]">
 					<form
 						action=""
