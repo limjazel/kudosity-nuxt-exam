3b82a0b17ab36d806783a3e396d8ebaf5644a192
Jazel Lim <lim.jazel@gmail.com>
2024-03-18 17:07:01 +0800

    Move global meta tags in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,4 +25,51 @@ export default defineNuxtConfig({
 			},
 		},
 	},
+	app: {
+		head: {
+			htmlAttrs: { lang: "en-US" },
+			link: [
+				{ rel: "viewport", content: "width=device-width,initial-scale=1.0" },
+				{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+				{
+					rel: "preconnect",
+					href: "https://fonts.gstatic.com",
+					crossorigin: true,
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Space+Grotesk:wght@300..700&display=swap",
+				},
+				{
+					rel: "preload",
+					href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Space+Grotesk:wght@300..700&display=swap",
+					as: "font",
+				},
+			],
+			icon: [
+				{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "manifest",
+					href: "/site.webmanifest",
+				},
+			],
+		},
+	},
 })
--- pages/index.vue
+++ pages/index.vue
@@ -24,49 +24,6 @@
 				charset: homepage.charSet,
 			},
 		],
-		htmlAttrs: { lang: homepage.lang },
-		link: [
-			{ rel: "viewport", content: homepage.viewport },
-			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-			{
-				rel: "preconnect",
-				href: "https://fonts.gstatic.com",
-				crossorigin: true,
-			},
-			{
-				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Space+Grotesk:wght@300..700&display=swap",
-			},
-			{
-				rel: "preload",
-				href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Space+Grotesk:wght@300..700&display=swap",
-				as: "font",
-			},
-		],
-		icon: [
-			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-			{
-				rel: "apple-touch-icon",
-				sizes: "180x180",
-				href: "/apple-touch-icon.png",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "32x32",
-				href: "/favicon-32x32.png",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "16x16",
-				href: "/favicon-16x16.png",
-			},
-			{
-				rel: "manifest",
-				href: "/site.webmanifest",
-			},
-		],
 	})
 
 	useSeoMeta({
